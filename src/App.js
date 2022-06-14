22385174aaf05cdd7355af52f302922eb2852c6c
tasnimraihan28 <tasnimraihan28@gmail.com>
2022-06-14 22:26:24 +0600

    tab

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [current, setCurrent] = useState("Item 1")
+    return (
+        <div className="App">
+            <nav className="tabNav">
+                <a
+                    href="#tab1"
+                    className={current==="Item 1"? "tabHdr selected":"tabHdr"}
+                    onClick={() => setCurrent("Item 1")}
+                >Item 1</a>
+                <a
+                    href="#tab2"
+                    className={current==="Item 2"? "tabHdr selected":"tabHdr"}
+                    onClick={() => setCurrent("Item 2")}
+                >Item 2</a>
+            </nav>
+
+            {/*  tab render*/}
+            {
+                current === "Item 1" && <div className="tabContent">
+                    {current}
+                </div>
+            }
+            {
+                current === "Item 2" && <div className="tabContent">
+                    {current}
+                </div>
+            }
+        </div>
+    );
 }
 
 export default App;
